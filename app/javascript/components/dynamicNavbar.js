8a4137f7f86da9a62978fad2045c923cb7bfd9a0
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2019-04-30 17:54:16 +0700

    hp 40%

---
+++ app/javascript/components/dynamicNavbar.js
@@ -0,0 +1,14 @@
+const navbar = document.getElementById('navbar')
+
+if(navbar){
+  window.addEventListener("scroll", function (event) {
+    var scroll = this.scrollY;
+    if (scroll == 0) {
+      navbar.style.backgroundColor = "transparent";
+      navbar.style.boxShadow = "none";
+    } else {
+      navbar.style.backgroundColor = "white";
+      navbar.style.boxShadow = "0px 10px 61px 0px rgba(42, 54, 84, 0.1)";
+    }
+  });
+}
