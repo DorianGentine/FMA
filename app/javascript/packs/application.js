8b1fd49b30ba5254eda37d489bb2cf65993b74f4
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2019-05-02 22:01:37 +0700

    Qui sommes nous

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,6 @@
 import "bootstrap";
 
+import "../components/dynamicNavbar";
+
 import { fetchFormulary } from "../components/formularies/new";
 fetchFormulary();
-
-import { navbar } from "../components/dynamicNavbar";
-navbar();
-
