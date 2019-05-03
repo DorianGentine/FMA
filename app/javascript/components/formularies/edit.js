bebfde520c33d10582a6a699fe1b0fbacfa52338
donatienrolland <donatien@rollandmail.com>
2019-05-03 12:04:36 +0700

    new et edit ok

--- app/javascript/components/formularies/edit.js
+++ app/javascript/components/formularies/edit.js
@@ -29,22 +29,22 @@ function updateFormulary(formulary){
 export { updateFormulary}
 
 
-const getEditAnswer = (questions) => {
-  const btns = document.querySelectorAll('.edit')
-  btns.forEach((btn) => {
-    btn.addEventListener('click', function(){
-      for (var i = 0; i < questions.length; i++) {
-        if (questions[i].set_up.position == btn.dataset.position) {
-          console.log(questions[i])
-          editAnswer(questions[i])
-        }
-      }
-    })
-  })
-}
+// const getEditAnswer = (questions) => {
+//   const btns = document.querySelectorAll('.edit')
+//   btns.forEach((btn) => {
+//     btn.addEventListener('click', function(){
+//       for (var i = 0; i < questions.length; i++) {
+//         if (questions[i].set_up.position == btn.dataset.position) {
 
-const editAnswer = (question) => {
-  const form = document.getElementById("formulary")
-  console.log(form)
-  console.log(question)
-}
+//           editAnswer(questions[i])
+//         }
+//       }
+//     })
+//   })
+// }
+
+// const editAnswer = (question) => {
+//   const form = document.getElementById("formulary")
+//   console.log(form)
+//   console.log(question)
+// }
--- app/javascript/components/formularies/new.js
+++ app/javascript/components/formularies/new.js
@@ -1,132 +1,66 @@
 import { scrollLastMessageIntoView } from "../../components/scroll";
 import {updateFormulary} from "../formularies/edit";
+import {
+  insertQuestion,
+  insertAnswer,
+  createInputAnswer,
+  insertSelectAnswer,
+  createSubmitBtn,
+  createEditBtn,
+  createLinkAnalyze,
+  setFormForFormulary
+} from "./composents";
+
 const form = document.getElementById("formulary")
-let question_send, answer_send, btn
 
-const insertQuestion = (question) => {
-  question_send = `<div class="message received">${question.set_up.position} - ${question.set_up.question}</div>`
+const getEditAnswer = (questions) => {
+  const btns = document.querySelectorAll('.edit')
+  btns.forEach((btn) => {
+    btn.addEventListener('click', function(){
+      for (var i = 0; i < questions.length; i++) {
+        if (questions[i].set_up.position == btn.dataset.position) {
+          form.innerHTML = ""
+          editAnswer(questions, questions[i])
+        }
+      }
+    })
+  })
 }
 
-const insertAnswer = (question) => {
-  answer_send = `<div class="message sent" data-columnName='${question.set_up.column_name}'>${question.answer}</div>`
+const editAnswer = (questions, question) => {
+  setQuestionsAnswer(questions, question)
 }
-
-const createInputAnswer = (question, div) => {
-  const input = document.createElement("input")
-  input.style.width = "70%"
-  input.classList = "form-control select optional"
-  input.id = `formulary_${question.set_up.column_name}`
-  input.name = `formulary[${question.set_up.column_name}]`
-  input.type = "text"
-  div.appendChild(input)
-}
-
-const insertSelectAnswer = (question, div) => {
-  const selectList = document.createElement("select")
-  selectList.style.width = "70%"
-  selectList.classList = "form-control select optional"
-  selectList.id = `formulary_${question.set_up.column_name}`
-  selectList.name = `formulary[${question.set_up.column_name}]`
-  div.appendChild(selectList)
-  var option = document.createElement("option")
-  option.text = question.set_up.placeholder
-  selectList.add(option)
-  for (var i=0; i < Object.keys(question.set_up.data).length; i++){
-    let option = document.createElement("option")
-    if (question.set_up.data.toString.call([])) {
-      option.value = i;
-    } else {option.value = question.set_up.data[i] }
-    option.text = question.set_up.data[i];
-    if (option.text == question.answer){
-      option.selected = "selected"
-    } else { }
-    selectList.add(option)
-  }
-}
-
-
-const createSubmitBtn = (div) => {
-  const submit = document.createElement("input")
-  submit.style.width = "30%"
-  submit.classList = "btn btn-primary"
-  submit.name = "commit"
-  submit.type = "submit"
-  div.appendChild(submit)
-}
-
-const createEditBtn = (data) => {
-  btn = `<button type="button" class="btn btn-light edit" data-position="${data.set_up.position}"><i class="fas fa-pencil-alt"></i></button>`
-}
-
-const createLinkAnalyze = () => {
-  const link = `<a class="nav-link btn-connexion" title="Voir mon analyse" href="/formularies/${form.dataset.id}" style="width: 100%;">Voir mon analyse</a>`
-  form.insertAdjacentHTML("beforeend", link)
-}
-
-const setFormForFormulary = (question) => {
-  const div = document.createElement('form')
-  if (form.dataset.id) {
-    div.classList = "simple_form edit_formulary"
-    div.id = `edit_formulary_${form.dataset.id}`
-    div.action = `/formularies/${form.dataset.id}`
-  } else {
-    div.classList = "simple_form new_formulary"
-    div.id = `new_formulary`
-    div.action = `/formularies`
-  }
-
-  div.noValidate = "novalidate"
-  // div.setAttribute("data-remote", false)
-  div.acceptCharset = "UTF-8"
-  div.method = "post"
-  div.style.display = "flex"
-  form.appendChild(div)
-
-  const input = document.createElement("input")
-  input.name = "_method"
-  if (form.dataset.id) {input.value = "patch"} else {input.value = "post"}
-  input.type = "hidden"
-  div.appendChild(input)
-  if (question.set_up.type === "select") {
-    insertSelectAnswer(question, div)
-
-  } else if (question.set_up.type === "input"){
-    createInputAnswer(question, div)
-  }
-  createSubmitBtn(div)
-}
-
 
 const insertQuestionAnswers = (data) => {
-  const datas = data.formulary.questions
-  for ( var i = 0; i < datas.length; i ++){
-    if (typeof datas[i].answer != 'string') { break; }
-    insertQuestion(datas[i])
-    createEditBtn(datas[i])
-    insertAnswer(datas[i])
-    var to_send = question_send + btn + answer_send
-    form.insertAdjacentHTML("beforeend", to_send);
+  const questions = data.formulary.questions
+  setQuestionsAnswer(questions)
+  scrollLastMessageIntoView()
+  getEditAnswer(data.formulary.questions)
+}
+
+const setQuestionsAnswer = (questions, question = null) => {
+  for ( var i = 0; i < questions.length; i ++){
+    if (question) { if (questions[i] === question) { break; }
+    } else { if (typeof questions[i].answer != 'string') { break; } }
+    insertQuestion(questions[i])
+    createEditBtn(questions[i])
+    insertAnswer(questions[i])
   }
-  if (data.formulary.questions[i]) {
-    const lastQuestion = `<div class="message received">${data.formulary.questions[i].set_up.position} - ${data.formulary.questions[i].set_up.question}</div>`
+  if (questions[i]) {
+    const lastQuestion = `<div class="message received">${questions[i].set_up.position} - ${questions[i].set_up.question}</div>`
     form.insertAdjacentHTML("beforeend", lastQuestion);
-    // insertSelectAnswer(data.formulary.questions[i])
-     setFormForFormulary(data.formulary.questions[i])
+     setFormForFormulary(questions[i])
   } else {
     createLinkAnalyze()
   }
-  scrollLastMessageIntoView()
-  updateFormulary(data.formulary)
 }
-
-
 
 function fetchFormulary(){
   const formulary_id = form.dataset.id
   if (formulary_id) {
-    var url = "http://localhost:3000/api/v1/formularies/"+ formulary_id +"/edit"
+    var url = "/api/v1/formularies/"+ formulary_id +"/edit"
   } else {
-    var url = "http://localhost:3000/api/v1/formularies/new"
+    var url = "/api/v1/formularies/new"
   }
   fetch(url)
     .then(response => response.json())
