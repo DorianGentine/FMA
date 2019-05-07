ee1b884ba3643dfb484b1390abca30acc9778272
donatienrolland <donatien@rollandmail.com>
2019-05-07 18:17:37 +0700

    test before action

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BaseController < ActionController::API
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   include Pundit
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
--- app/controllers/api/v1/formularies_controller.rb
+++ app/controllers/api/v1/formularies_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::FormulariesController < Api::V1::BaseController
   before_action :set_visitor, only: [:new, :edit]
-  skip_before_action :authenticate_user!, only: [ :update, :create, :new, :edit]
-  # skip_after_action :verify_policy_scoped, only: [:update, :create, :new]
-
-  skip_after_action :verify_authorized, only: [:update, :create, :new, :show]
+  after_action :verify_authorized, except: [:update, :create, :new, :show, :edit]
+  skip_after_action :verify_policy_scoped, only: [:update, :create, :new, :edit]
 
 
   def show
