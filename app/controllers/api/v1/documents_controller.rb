1acd7c12f1495ede728814eb964310df5c2ef9a1
donatienrolland <donatien@rollandmail.com>
2019-05-15 11:42:32 +0700

    create document

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,13 +1,15 @@
 class Api::V1::DocumentsController < Api::V1::BaseController
-  skip_before_action :verify_authenticity_token
+
   before_action :authenticate_user!
 
   def create
     document = Document.new
     project = Project.find(params[:project_id])
+    user = User.find(params[:user_id])
     p "////: project is => #{project}"
-    document.file = params["uploaded_file"]
-    p "Document with file => #{document}"
+    p "PARAMS =>    >>>>>> #{params["uploaded_image"]}"
+    document.file = params["uploaded_image"]
+    p "Document with file => #{document.file}"
     document.project = project
     p "Document with project => #{document}"
     if document.save
@@ -16,6 +18,8 @@ class Api::V1::DocumentsController < Api::V1::BaseController
     else
       p " ////// Document is save"
     end
+    # CHANGE POLICY FOR PROJECT
+    authorize user
   end
 end
 
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -10,4 +10,7 @@ class UserPolicy < ApplicationPolicy
     # true if user.admin
     # true if record.user_projects.where(user: user)
   end
+  def create?
+    true
+  end
 end
