f097737778b1e5868a4309e474ca23654ecf0a2a
donatienrolland <donatien@rollandmail.com>
2019-05-30 11:06:43 +0700

    modif

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::DocumentsController < Api::V1::BaseController
   before_action :authenticate_user!
 
   def update
+    p "/// Im in"
+    p "params is #{params}"
+    p "params Uplaod is #{params["uploaded_image"]}"
     document = Document.find(params[:id])
+    p "Document is #{document}"
     project = document.project
     document.file = params["uploaded_image"]
     if document.save
