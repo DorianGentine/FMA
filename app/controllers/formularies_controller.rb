d798c2e84df5269220afc4d7e2b7b87926e2f048
donatienrolland <donatien@rollandmail.com>
2019-06-08 16:28:59 +0700

    ok

--- app/controllers/formularies_controller.rb
+++ app/controllers/formularies_controller.rb
@@ -5,6 +5,7 @@ class FormulariesController < ApplicationController
   def show
     session[:formulary_id] = params[:id]
     @formulary = Formulary.find(params[:id])
+    @formulary.solutions
     @visitor = @formulary.visitor
     @testing_solutions = Solution.all.map { |x| [x.id, TestSolution.new(x).test_solution_form] }.to_h.sort.to_h
     authorize @formulary
--- db/migrate/20190326075402_create_projects.rb
+++ db/migrate/20190326075402_create_projects.rb
@@ -2,6 +2,7 @@ class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
       t.integer :step
+      t.string :appointment
       t.timestamps
     end
   end
