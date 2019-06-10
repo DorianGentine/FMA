3b043597d590b8a0974eddf31b66aea284714243
donatienrolland <donatien@rollandmail.com>
2019-06-10 13:05:59 +0700

    solve faltpickr

--- app/javascript/components/flatpicker.js
+++ app/javascript/components/flatpicker.js
@@ -1,27 +1,28 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
-// require("flatpickr/dist/themes/material_blue.css");
-// import { French } from "flatpickr/dist/l10n/fr.js"
+require("flatpickr/dist/themes/material_blue.css");
+import { French } from "flatpickr/dist/l10n/fr.js"
 
 
 const flatpicker = () => {
-  console.log("YEs? ")
   flatpickr(".datepicker", {
-    // "locale": French,
-    // dateFormat: "d/m/Y",
+    "locale": French,
+    dateFormat: "d/m/Y",
     // defaultDate: "12/12/1953"
   })
   setFmaColor()
 }
 
 const setFmaColor = () => {
-  console.log("YEs? ")
   if (document.querySelector(".datepicker")) {
     document.querySelector(".flatpickr-weekdays").style.backgroundColor = "#2675e5"
     document.querySelector(".flatpickr-weekday").style.backgroundColor = "#2675e5"
     document.querySelectorAll(".flatpickr-weekday").forEach((selected)=> selected.style.backgroundColor = "#2675e5")
     document.querySelector(".flatpickr-month").style.backgroundColor = "#2675e5"
     document.querySelector(".flatpickr-monthDropdown-months").style.backgroundColor = "#2675e5"
+    if (document.querySelector(".selected")) {
+      document.querySelector(".selected").style.backgroundColor = "#2675e5"
+    }
   }
 }
 
