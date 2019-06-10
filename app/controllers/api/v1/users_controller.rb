3a4109da4df0c9a872adc098b9a3e6e3aaf94f98
donatienrolland <donatien@rollandmail.com>
2019-06-10 14:05:39 +0700

    preparing for password update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
-  before_action :authenticate_user!
+  acts_as_token_authentication_handler_for User, except: [ :show ]
+  # before_action :authenticate_user!
   before_action :set_user, only: [:show, :update]
 
   def show
@@ -15,10 +16,23 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def update
-
+    if @user.update(user_params)
+      render :show
+    else
+      render_error
+    end
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+
+  def render_error
+    render json: { errors: @user.errors.full_messages },
+      status: :unprocessable_entity
+  end
 
   def set_user
     @user = User.find(params[:id])
