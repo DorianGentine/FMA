91e1231388add9457bd1b9822b9b6ad24dc63ef5
donatienrolland <donatien@rollandmail.com>
2019-06-28 17:56:58 +0200

    modif param message

--- app/controllers/api/v1/calendlies_controller.rb
+++ app/controllers/api/v1/calendlies_controller.rb
@@ -12,13 +12,13 @@ class Api::V1::CalendliesController < Api::V1::BaseController
     @user = User.find(1)
     p "////////////// data #{params[:data]}"
     p "////////////// item #{params[:data][:item]}"
-    p "////////////// author #{params[:data][:item][:conversation_parts][:author]}"
+    p "////////////// author #{params[:data][:item][:conversation_parts][:conversation_parts][:author]}"
     p "////////////// links #{params[:data][:item][:links]}"
     message = Message.last
     if message.unread > 0
       message.unread = message.unread + 1
     else
-      author = params[:data][:item][:conversation_parts][:author]
+      author = params[:data][:item][:conversation_parts][:conversation_parts][:author]
       message.update(url: params[:data][:item][:links][:conversation_web], email: author[:email], full_name: author[:name])
     end
     message.save
