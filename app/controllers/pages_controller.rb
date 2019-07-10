ab3df76f258d9a35855832cae8468a702419a15d
donatienrolland <donatien@rollandmail.com>
2019-07-10 16:36:40 +0700

    test forma date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -33,8 +33,8 @@ class PagesController < ApplicationController
     p "/////event_start_time #{params["event_start_time"]}"
     p "/////test time #{ DateTime.parse(params['event_start_time'])}"
 
-    # appointment = Time.parse(params["event_start_time"])
-    app = l(DateTime.parse(params["event_start_time"]), :format => '%A %d %B %Y à %Hh%M')
+    app = Time.parse(params["event_start_time"])
+    # app = l(DateTime.parse(params["event_start_time"]), :format => '%A %d %B %Y à %Hh%M')
     p "/////appointment #{app}"
     if current_user
       @user = current_user
